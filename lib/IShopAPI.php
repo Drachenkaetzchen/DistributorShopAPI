8374d39b437fec876605a89a19702ea775e72e79
Lars Kumbier <lars.kumbier@dl.srh.de>
2012-01-06 22:30:23 +0100

    fixed: wrong phpdoc-key

--- lib/IShopAPI.php
+++ lib/IShopAPI.php
@@ -2,6 +2,12 @@
 
 interface IShopAPI {
 	/**
+	 * Return the Distributor name
+	 * @return string
+	 */
+	public function GetDistributorName();
+
+	/**
 	 * Searches an article by it's unique identifier in the shop in question.
 	 * Returns a boolean false, if the article was not found, and an instance
 	 * of an Article-Class, if found.
